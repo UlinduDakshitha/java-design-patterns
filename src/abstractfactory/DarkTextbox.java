fe1b9edc2f0d21f2f23550a2dc9b8286da55203e
Bandara W.M.U.D. <166277247+UlinduDakshitha@users.noreply.github.com>
2026-03-23 11:15:53 +0530

    update

---
+++ src/abstractfactory/DarkTextbox.java
@@ -0,0 +1,7 @@
+package abstractfactory;
+
+class DarkTextbox implements TextBox {
+    public void paint() {
+        System.out.println("Dark Textbox");
+    }
+}
---
+++ src/abstractfactory/LightTextbox.java
@@ -0,0 +1,7 @@
+package abstractfactory;
+
+class LightTextbox implements TextBox {
+    public void paint() {
+        System.out.println("Light Textbox");
+    }
+}
