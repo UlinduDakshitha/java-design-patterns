662ba25d12480c0c16781d49c68823dfe2f870cc
Bandara W.M.U.D. <166277247+UlinduDakshitha@users.noreply.github.com>
2026-03-23 11:07:16 +0530

    update

---
+++ src/abstractfactory/DarkThemeFactory.java
@@ -0,0 +1,12 @@
+package abstractfactory;
+
+public class DarkThemeFactory implements GUIFactory {
+
+    public Button createButton() {
+        return new DarkButton();
+    }
+
+    public Checkbox createCheckbox() {
+        return new DarkCheckbox();
+    }
+}
