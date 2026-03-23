052ef9524d87967f425eb24c3f1661ea1109896b
Bandara W.M.U.D. <166277247+UlinduDakshitha@users.noreply.github.com>
2026-03-23 11:12:05 +0530

    update

---
+++ src/abstractfactory/Main.java
@@ -0,0 +1,22 @@
+package abstractfactory;
+
+public class Main {
+    public static void main(String[] args) {
+
+        GUIFactory factory;
+
+        String theme = "dark"; // change to "light" to test
+
+        if (theme.equalsIgnoreCase("dark")) {
+            factory = new DarkThemeFactory();
+        } else {
+            factory = new LightThemeFactory();
+        }
+
+        Button button = factory.createButton();
+        Checkbox checkbox = factory.createCheckbox();
+
+        button.paint();
+        checkbox.paint();
+    }
+}
