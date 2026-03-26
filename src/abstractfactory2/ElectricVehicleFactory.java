6edc666a02430db301b6613165bdb54e213b5891
Bandara W.M.U.D. <166277247+UlinduDakshitha@users.noreply.github.com>
2026-03-26 20:29:25 +0530

    update

---
+++ src/abstractfactory2/ElectricVehicleFactory.java
@@ -0,0 +1,10 @@
+package abstractfactory2;
+class ElectricVehicleFactory implements VehicleFactory {
+    public Car createCar() {
+        return new ElectricCar();
+    }
+
+    public Bike createBike() {
+        return new ElectricBike();
+    }
+}
---
+++ src/abstractfactory2/PetrolBike.java
@@ -0,0 +1,7 @@
+package abstractfactory2;
+
+public class PetrolBike implements Bike{
+    public void manufacture() {
+        System.out.println("Manufacturing Petrol Car");
+    }
+}
